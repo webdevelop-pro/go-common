595f20990fb9b67b9e5fbdca053cf7e2ab99656d
Georgy Shapchits <georgy.schapchits@synesis.ru>
2021-09-20 15:46:26 +0300

    Add github implementation for git client

---
+++ git/git.go
@@ -0,0 +1,48 @@
+package git
+
+import (
+	"context"
+
+	"github.com/google/go-github/v39/github"
+	"golang.org/x/oauth2"
+)
+
+type Client interface {
+	getCommit(repo, sha string) (GitCommit, error)
+}
+
+type GitCommit struct {
+	Author  string
+	URL     string
+	Message string
+}
+
+type Github struct {
+	AccessToken string `json:"access_token"`
+	RepoOwner   string `json:"repo_owner"`
+}
+
+func (g Github) getCommit(repo, sha string) (GitCommit, error) {
+	ctx := context.Background()
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: g.AccessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	commit, _, err := client.Git.GetCommit(
+		ctx,
+		g.RepoOwner,
+		repo,
+		sha,
+	)
+
+	return GitCommit{
+		Author:  *commit.Author.Name,
+		URL:     *commit.URL,
+		Message: *commit.Message,
+	}, err
+}
