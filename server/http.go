a35b02a9352679b6279081b1f50bf1da2145dc9b
adams <adams@UNATCO.unatco.gov>
2023-10-15 21:27:06 +0300

    log incoming requests

--- server/http.go
+++ server/http.go
@@ -72,6 +72,7 @@ func NewHttpServer(e *echo.Echo, l logger.Logger, cfg *Config, authTool *middlew
 	e.Use(middleware.SetLogger)
 	e.Use(middleware.SetRequestTime)
 	e.Use(middleware.SetIPAddress)
+	e.Use(middleware.LogRequests)
 	// Trace ID middleware generates a unique id for a request.
 	e.Use(echoMW.RequestIDWithConfig(echoMW.RequestIDConfig{
 		RequestIDHandler: func(c echo.Context, requestID string) {
---
+++ server/middleware/log_request.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/webdevelop-pro/go-common/logger"
+)
+
+// Writes down every request to the log file
+// Usefull for debagging
+func LogRequests(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		// create sub logger
+		log := logger.NewComponentLogger("log-requests", c)
+		// enrich context
+		raw, _ := io.ReadAll(c.Request().Body)
+		c.Request().Body = io.NopCloser(bytes.NewReader(raw))
+		log.Trace().Interface("headers", c.Request().Header).Msg("headers")
+		log.Trace().Interface("body", string(raw)).Msg("raw body")
+		// next handler
+		return next(c)
+	}
+}
+
+// ToDo
+// Ability to set up log level in config file
