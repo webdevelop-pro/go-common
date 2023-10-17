6d9fb0a98fc448d62af64a236b72290a43c9a2e9
Vlad <vladka@webdevelop.pro>
2023-10-17 14:43:37 +0300

    small fix for log requests

--- server/middleware/log_request.go
+++ server/middleware/log_request.go
@@ -12,13 +12,17 @@ import (
 // Usefull for debagging
 func LogRequests(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		// create sub logger
-		log := logger.NewComponentLogger("log-requests", c)
-		// enrich context
-		raw, _ := io.ReadAll(c.Request().Body)
-		c.Request().Body = io.NopCloser(bytes.NewReader(raw))
-		log.Trace().Interface("headers", c.Request().Header).Msg("headers")
-		log.Trace().Interface("body", string(raw)).Msg("raw body")
+
+		// ignore healthcheck requests
+		if c.Request().URL.Path != "/healthcheck" {
+			// create sub logger
+			log := logger.NewComponentLogger("log-requests", c)
+			// enrich context
+			raw, _ := io.ReadAll(c.Request().Body)
+			c.Request().Body = io.NopCloser(bytes.NewReader(raw))
+			log.Trace().Interface("headers", c.Request().Header).Msg("headers")
+			log.Trace().Interface("body", string(raw)).Msg("raw body")
+		}
 		// next handler
 		return next(c)
 	}
