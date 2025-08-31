ca0ba9f9c1ef036ef3ef4b2cd47c7d2b3e4b51b5
Vlad <vladka@webdevelop.pro>
2025-08-31 16:53:44 +0000

    go-common multi build

--- docker/build-deploy.sh
+++ docker/build-deploy.sh
@@ -18,7 +18,7 @@ audit)
   BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
   GIT_COMMIT=`git rev-parse --short HEAD`
   echo $BRANCH_NAME, $GIT_COMMIT
-  docker build -t cr.webdevelop.pro/$COMPANY_NAME/$SERVICE_NAME:$GIT_COMMIT -t cr.webdevelop.pro/$COMPANY_NAME/$SERVICE_NAME:latest-dev -t docker.io/webdeveloppro/$SERVICE_NAME:$GIT_COMMIT -t docker.io/webdeveloppro/$SERVICE_NAME:latest-dev -t cr.webdevelop.biz/$COMPANY_NAME/$SERVICE_NAME:latest-dev --platform=linux/amd64 .
+  docker buildx build --platform linux/amd64,linux/arm64 -t cr.webdevelop.pro/$COMPANY_NAME/$SERVICE_NAME:$GIT_COMMIT -t cr.webdevelop.pro/$COMPANY_NAME/$SERVICE_NAME:latest-dev -t docker.io/webdeveloppro/$SERVICE_NAME:$GIT_COMMIT -t docker.io/webdeveloppro/$SERVICE_NAME:latest-dev -t cr.webdevelop.biz/$COMPANY_NAME/$SERVICE_NAME:latest-dev --platform=linux/amd64 .
   # snyk container test cr.webdevelop.pro/$COMPANY_NAME/$SERVICE_NAME:$GIT_COMMIT
   if [ $? -ne 0 ]; then
     echo "===================="
